b1c3895bf2e54e9443d908d19e9ae7316b94a5c9
Tanishq Parihar <tanishqparihar3@gmail.com>
2023-12-26 15:39:48 +0530

    Create Arrays.cpp

---
+++ STL/Arrays.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    array<int, 5> arr= {1,2,3,4,5};
+    arr.fill(10);
+
+    // arr.at(index);
+    // int it;
+    // for(auto it= arr.begin(); it!=arr.end(); it++){
+    //     cout<<*it <<" ";
+        
+    // }
+
+//    string s= "anushka";
+//    for(auto c:s){
+//     cout<<c<<" ";
+//    }
+
+    return 0;
+}
+//rbegin(), rend(), end(), begin(), size(), back(), front()
+
