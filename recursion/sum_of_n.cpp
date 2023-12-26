e3f86558a03edfcbb1ff50c5e80ba91b831447c9
Tanishq Parihar <tanishqparihar3@gmail.com>
2023-12-26 13:06:33 +0530

    Create sum_of_n.cpp

---
+++ recursion/sum_of_n.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum(int a){
+    if(a==0){
+        return 0;
+    }
+    return a+ sum(a-1);}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<sum(n);
+}
