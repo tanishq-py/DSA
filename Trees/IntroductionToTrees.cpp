bbf54920e5fe69e3cb39396efb628d5e1de290aa
Tanishq Parihar <tanishqparihar3@gmail.com>
2023-12-29 13:47:00 +0530

    Create IntroductionToTrees.cpp

---
+++ Trees/IntroductionToTrees.cpp
@@ -0,0 +1,7 @@
+// root -> children -> node
+
+//full binary tree - either has 0 or 2 children
+//complete binary trees - all levels are completely filled except last level & the last level m left as posssible
+//perfect binary tree- all node at same level
+//balanced binary tree - height of tree at max log(n)
+//degenrate tree-
