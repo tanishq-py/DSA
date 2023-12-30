0637c8a218f9e9077b2e824a9652ccd08cd25681
Tanishq Parihar <tanishqparihar3@gmail.com>
2023-12-30 23:09:56 +0530

    Update print_n_times.cpp

--- recursion/print_n_times.cpp
+++ recursion/print_n_times.cpp
@@ -6,3 +6,15 @@ vector<string> printNTimes(int n) {
 }
 
 
+//print 1 to n
+vector<int> printNos(int x) {
+    static int i = 1;  // Make i static to persist its value across calls
+    if (x <= 0) {
+        return {};
+    }
+    cout << i << " ";
+    i++;  // Increment after printing
+    return printNos(x - 1);
+}
+
+
