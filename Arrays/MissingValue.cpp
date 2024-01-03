2535c7ee29f23cd2bd9eaa09bab62367095a1846
Tanishq Parihar <tanishqparihar3@gmail.com>
2024-01-03 22:26:09 +0530

    Create MissingValue.cpp

---
+++ Arrays/MissingValue.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int missingNumber(std::vector<int>& nums) {
+        int n = nums.size();
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int i = 0; i < n; i++) {
+            actualSum += nums[i];
+        }
+        return expectedSum - actualSum;
+    }
+};
