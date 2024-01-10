b4f9c4bd25f7267429be976a236c67b50106f0b5
Tanishq Parihar <tanishqparihar3@gmail.com>
2024-01-10 14:49:55 +0530

    Create 2_NthRoot.cpp

---
+++ BinarySearch/BSonAnswers/2_NthRoot.cpp
@@ -0,0 +1,48 @@
+//Given two numbers N and M, find the Nth root of M.
+//The nth root of a number M is defined as a number X when raised to the power N equals M.
+//If the ‘nth root is not an integer, return -1.
+
+//brute force
+long long func(int b, int exp) {
+    long long  ans = 1;
+    long long base = b;
+    while (exp > 0) {
+        if (exp % 2) {
+            exp--;
+            ans = ans * base;
+        }
+        else {
+            exp /= 2;
+            base = base * base;
+        }
+    }
+    return ans;
+}
+
+
+//optimal solution
+int func(int mid, int n, int m) {
+    long long ans = 1;
+    for (int i = 1; i <= n; i++) {
+        ans = ans * mid;
+        if (ans > m) return 2;
+    }
+    if (ans == m) return 1;
+    return 0;
+}
+
+int NthRoot(int n, int m) {
+    //Use Binary search on the answer space:
+    int low = 1, high = m;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int midN = func(mid, n, m);
+        if (midN == 1) {
+            return mid;
+        }
+        else if (midN == 0) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
