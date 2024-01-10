1d6a8c2892b74b720877f89adddcc7a56abebb7f
Tanishq Parihar <tanishqparihar3@gmail.com>
2024-01-10 11:38:09 +0530

    Create 8_TimesArrayRotated.cpp

---
+++ BinarySearch/8_TimesArrayRotated.cpp
@@ -0,0 +1,59 @@
+// //Given an integer array arr of size N, sorted in ascending order (with distinct values). 
+// Now the array is rotated between 1 to N times which is unknown. 
+// Find how many times the array has been rotated. 
+
+
+//brute force
+int findKRotation(vector<int> &arr) {
+    int n = arr.size(); //size of array.
+    int ans = INT_MAX, index = -1;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < ans) {
+            ans = arr[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+//optimal aproach
+int findKRotation(vector<int> &arr) {
+    int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+    int index = -1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        //search space is already sorted
+        //then arr[low] will always be
+        //the minimum in that search space:
+        if (arr[low] <= arr[high]) {
+            if (arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
+            break;
+        }
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            // keep the minimum:
+            if (arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
+
+            // Eliminate left half:
+            low = mid + 1;
+        }
+        else { //if right part is sorted:
+
+            // keep the minimum:
+            if (arr[mid] < ans) {
+                index = mid;
+                ans = arr[mid];
+            }
+            // Eliminate right half:
+            high = mid - 1;
+        }
+    }
+    return index;
+}
