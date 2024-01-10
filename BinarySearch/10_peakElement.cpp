4b638c79f2ab230a5d196ba77ec46359b61299b9
Tanishq Parihar <tanishqparihar3@gmail.com>
2024-01-10 14:20:24 +0530

    Create 10_peakElement.cpp

---
+++ BinarySearch/10_peakElement.cpp
@@ -0,0 +1,46 @@
+// Given an array of length N. Peak element is defined as the element greater than both of its neighbors.
+// Formally, if ‘arr[i]’ is the peak element, ‘arr[i – 1]’ < ‘arr[i]’ and ‘arr[i + 1]’ < ‘arr[i]’. 
+// Find the index(0-based) of a peak element in the array. If there are multiple peak numbers, return the index of any peak number.
+
+//brute force
+int findPeakElement(vector<int> &arr) {
+    int n = arr.size(); //Size of array.
+
+    for (int i = 0; i < n; i++) {
+        //Checking for the peak:
+        if ((i == 0 || arr[i - 1] < arr[i])
+                && (i == n - 1 || arr[i] > arr[i + 1])) {
+            return i;
+        }
+    }
+    // Dummy return statement
+    return -1;
+}
+
+//optimal solution
+int findPeakElement(vector<int> &arr) {
+    int n = arr.size(); //Size of array.
+
+    // Edge cases:
+    if (n == 1) return 0;
+    if (arr[0] > arr[1]) return 0;
+    if (arr[n - 1] > arr[n - 2]) return n - 1;
+
+    int low = 1, high = n - 2;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //If arr[mid] is the peak:
+        if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
+            return mid;
+        // If we are in the left:
+        if (arr[mid] > arr[mid - 1]) low = mid + 1;
+
+        // If we are in the right:
+        // Or, arr[mid] is a common point:
+        else high = mid - 1;
+    }
+    // Dummy return statement
+    return -1;
+}
+
