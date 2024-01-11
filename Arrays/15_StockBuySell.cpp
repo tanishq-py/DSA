5183cf297082d3dd428fc68058d7327afbfbfbfc
Tanishq Parihar <tanishqparihar3@gmail.com>
2024-01-11 14:11:12 +0530

    Create 15_StockBuySell.cpp

---
+++ Arrays/15_StockBuySell.cpp
@@ -0,0 +1,33 @@
+//ou are given an array of prices where prices[i] is the price of a given stock on an ith day.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+//brute force
+int maxProfit(vector<int> &arr) {
+    int maxPro = 0;
+    int n = arr.size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[i]) {
+            maxPro = max(arr[j] - arr[i], maxPro);
+            }
+        }
+        }
+
+    return maxPro;
+}
+
+//optimal solution
+int maxProfit(vector<int> &arr) {
+    int maxPro = 0;
+    int n = arr.size();
+    int minPrice = INT_MAX;
+
+    for (int i = 0; i < arr.size(); i++) {
+        minPrice = min(minPrice, arr[i]);
+        maxPro = max(maxPro, arr[i] - minPrice);
+    }
+    
+    return maxPro;
+}
