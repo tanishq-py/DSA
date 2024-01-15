c09335c591a5a484264f8e71de1efeb5ee719f45
Tanishq Parihar <tanishqparihar3@gmail.com>
2024-01-15 18:26:39 +0530

    Update 3_Palindrome.cpp

--- Maths/Bits/3_Palindrome.cpp
+++ Maths/Bits/3_Palindrome.cpp
@@ -1,13 +1,21 @@
 //Check whether a given number ’n’ is a palindrome number
-bool palindrome(int n)
-{
-    int rev=0;
-    int org=n;
-    while(n!=0){
-        int digit=n%10;
-        rev= rev*10+ digit;
-        n=n/10;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long int reverse=0;
+        long long int orginal=x;
+        while(x!=0){
+            reverse=reverse*10+(x%10);
+            x=x/10;
+        }
+        if(orginal<0){
+        return false;
+        }
+        
+        if(orginal==reverse){
+            return true;
+        }
+
+        return false;
     }
-    if(rev==org) return true;
-    return false;
-}
+};
