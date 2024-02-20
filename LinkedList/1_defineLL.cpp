e969149865fef64458bddce0a0efc03d68aae9f0
Tanishq Parihar <tanishqparihar3@gmail.com>
2024-02-20 22:39:47 +0530

    Update 1_defineLL.cpp

--- LinkedList/1_defineLL.cpp
+++ LinkedList/1_defineLL.cpp
@@ -1,65 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Node {
-    public: 
-    int data;
-    Node* next;
 
-    Node(int data1, Node* next1){
-        data = data1;
+class node{
+    public:
+    int data;
+    node *next;
+    public:
+    node(int data1, node *next1){
+        data= data1;
         next = next1;
     }
-
-    Node(int data1){
-        data= data1;
-        next= nullptr;
+    node(int data1){
+        data=data1;
+        next=NULL;
     }
-
 };
 
 int main(){
-    vector<int> arr= {2,5,6,7};
-    Node* y= new Node(arr[0]);
-    cout<<y->data;
-    return 0;
+    vector<int> arr={2,5,4,6};
+    node *y= new node(arr[0]);
+    cout<<y<<endl;
+    cout<<y->data<<endl;
 }
-
-//convert array to LL
-Node* constructLL(vector<int>& arr) {
-    if (arr.empty()) {
-        return nullptr;  
-    }
-    Node* head = new Node(arr[0]);
-    Node* mover = head;
-    for (int i = 1; i < arr.size(); ++i) {
-        Node* temp = new Node(arr[i]);
-        mover->next = temp;
-        mover = temp;
-    }
-    return head;  
-}
-
-//length of LL
-int length(Node *head)
-{
-	  int cnt=0;
-        Node* temp =head;
-        while(temp){
-            temp= temp->next;
-            cnt++;
-        }
-        return cnt;
-}
-
-
-//search in LL
-int searchInLinkedList(Node<int> *head, int k) {
-    Node<int>*temp = head;
-    while(temp){
-        if(temp->data==k) return 1;
-        temp= temp->next;
-    }
-    return 0;
-}
-
 
